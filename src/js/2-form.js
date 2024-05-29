2720d55849f648bd2ea12a3a1fb29c266229dd30
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-05-29 18:56:30 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,32 @@
-const form = document.querySelector('form');
-const input = form.querySelector('input');
-const textArea = form.querySelector('textarea');
 const formData = {
   email: '',
   message: '',
 };
-
-input.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
-textArea.value = JSON.parse(
-  localStorage.getItem('feedback-form-state')
-).message;
-
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const textArea = form.querySelector('textarea');
+if (localStorage.getItem('feedback-form-state')) {
+  formData.email = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).email;
+  formData.message = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
+}
 form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
+input.value = formData.email;
+textArea.value = formData.message;
+
 form.addEventListener('submit', e => {
   e.preventDefault();
+  if (!input.value || !textArea.value) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.clear();
+    form.reset();
+  }
 });
