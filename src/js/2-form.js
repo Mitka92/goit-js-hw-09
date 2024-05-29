474c467f88763ef442388e662b946bbb562949fb
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-05-29 20:13:25 +0300

    2-form.js edit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,26 +3,23 @@ const formData = {
   message: '',
 };
 const form = document.querySelector('form');
-const input = form.querySelector('input');
-const textArea = form.querySelector('textarea');
-if (localStorage.getItem('feedback-form-state')) {
-  formData.email = JSON.parse(
-    localStorage.getItem('feedback-form-state')
-  ).email;
-  formData.message = JSON.parse(
-    localStorage.getItem('feedback-form-state')
-  ).message;
+const emailInputElem = form.querySelector('input');
+const messageInputElem = form.querySelector('textarea');
+const savedFormData = localStorage.getItem('feedback-form-state');
+if (savedFormData) {
+  formData.email = JSON.parse(savedFormData).email;
+  formData.message = JSON.parse(savedFormData).message;
 }
 form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
-input.value = formData.email;
-textArea.value = formData.message;
+emailInputElem.value = formData.email;
+messageInputElem.value = formData.message;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  if (!input.value || !textArea.value) {
+  if (!emailInputElem.value || !messageInputElem.value) {
     alert('Fill please all fields');
   } else {
     console.log(formData);
